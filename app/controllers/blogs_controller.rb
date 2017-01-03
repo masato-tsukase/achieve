47b7fb3d245371da1b57b8397bb01cbe7a73015d
Tsukase Masato <tsukasemasato@Tsukase-no-MacBook-Air.local>
2017-01-03 18:10:50 +0900

    “kadai11”

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -13,14 +13,14 @@ class BlogsController < ApplicationController
       @blog = Blog.new
     end
   end
-  
+
   def edit
   end
-  
+
   def update
     if @blog.update(blogs_params)
      redirect_to blogs_path, notice: "ブログを更新しました！"
-    else 
+    else
       render 'edit'
     end
   end
@@ -30,26 +30,27 @@ class BlogsController < ApplicationController
     @blog.user_id = current_user.id
     if @blog.save
      redirect_to blogs_path, notice: "ブログを作成しました！"
+     NoticeMailer.sendmail_blog(@blog).deliver
     else
       render 'new'
     end
   end
-  
+
   def destroy
     @blog.destroy
     redirect_to blogs_path, notice: "ブログを削除しました！"
   end
-  
+
   def confirm
     @blog = Blog.new(blogs_params)
     render :new if @blog.invalid?
   end
-  
+
   private
   def blogs_params
   params.require(:blog).permit(:title, :content)
   end
-    
+
   def set_blog
   @blog = Blog.find(params[:id])
   end
