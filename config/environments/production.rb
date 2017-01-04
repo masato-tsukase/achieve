4cf288acacd2756ecea42b01285e96d1ca18ce4d
Tsukase Masato <tsukasemasato@Tsukase-no-MacBook-Air.local>
2017-01-04 10:58:14 +0900

    11

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,9 +76,11 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: ' frozen-dusk-70900' }
 end
 
-config.action_mailer.default_url_options = { host: 'masax0408' }
+
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings =
 {
