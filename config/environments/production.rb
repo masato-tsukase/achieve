0e7d9948d76ee160822d3c326fc47068c6c32a3c
Tsukase Masato <tsukasemasato@Tsukase-no-MacBook-Air.local>
2017-01-04 09:46:33 +0900

    11

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 end
 
-config.action_mailer.default_url_options = { host: 'masax0408@yahoo.co.jp' }
+config.action_mailer.default_url_options = { host: 'masax0408' }
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings =
 {
