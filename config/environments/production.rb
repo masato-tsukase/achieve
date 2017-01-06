de774f22b899d02187e8b07913d309c579403e49
Tsukase Masato <tsukasemasato@Tsukase-no-MacBook-Air.local>
2017-01-06 10:34:30 +0900

    “11”

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: ' frozen-dusk-70900' }
+  config.action_mailer.default_url_options = { host: 'https://frozen-dusk-70900.herokuapp.com/' }
 end
 
 
