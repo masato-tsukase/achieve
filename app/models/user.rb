9a2e19919a2bd945b3a7859d61f68e8cf63f9b79
Tsukase Masato <tsukasemasato@Tsukase-no-MacBook-Air.local>
2017-01-29 22:28:01 +0900

    編集リンク削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,15 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable
-# 省略
 
   has_many :blogs, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
+
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_relationships, source: :follower
 
   mount_uploader :avatar, AvatarUploader
 
